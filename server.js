97d78c5b24de82f3ff30059ef43b5482c5ad7ddd
Erwin Jocosing <erwinjocosing@gmail.com>
2023-06-21 18:05:34 -0700

    updated server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Server setup
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 // Static middleware
